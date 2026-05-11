63f5890a727b5a5c2487e65233ea1e7c6bfabbbc
Jenny Tassotto <jennytassotto@gmail.com>
2026-05-11 11:38:58 +0200

    Fix build : augmente limite PWA cache à 4MB

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // 4 MB
+      },
       manifest: {
         name: 'Chipeur',
         short_name: 'Chipeur',
